e893ff2bb3be4037780881896091673203447090
preeti-prakash <preetiprakashkandregula@gmail.com>
2024-12-08 15:04:05 -0500

    books - get api

--- books.py
+++ books.py
@@ -2,6 +2,16 @@ from fastapi import FastAPI
 
 app = FastAPI()         #allows uvicorn to identify that we are creating a web app of fastapi
 
-@app.get("/api-endpoint")
-async def first_api():
-    return {"message": "Hello Preeti"}
+
+BOOKS = [
+    {'title': 'Title One', 'author': 'Author One', 'category': 'science'},
+    {'title': 'Title Two', 'author': 'Author Two', 'category': 'science'},
+    {'title': 'Title Three', 'author': 'Author Three', 'category': 'history'},
+    {'title': 'Title Four', 'author': 'Author Four', 'category': 'math'},
+    {'title': 'Title Five', 'author': 'Author Five', 'category': 'math'},
+    {'title': 'Title Six', 'author': 'Author Two', 'category': 'math'}
+]
+
+@app.get("/BOOKS")
+async def read_all_books():
+    return BOOKS
