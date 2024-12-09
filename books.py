f89b2c3f2dbb37e5358890bf0b542018d6f98ffb
preeti-prakash <preetiprakashkandregula@gmail.com>
2024-12-09 16:00:28 -0500

    put - update method

--- books.py
+++ books.py
@@ -91,8 +91,32 @@ async def read_author_category_by_query(book_author: str,category: str):
 @app.post("/books/create_book")
 async def create_book(new_book = Body()):
     BOOKS.append(new_book)
-    
+
+# Request Body{
+#     "title": "Title Seven",
+#     "author": "Author Two",
+#     "category": "Math"
+#   }
 # Request URL: http://localhost:8000/books/create_book
 # Response: null
 # check the new book calling the get api of all books and check
-    
+
+
+
+# PUT
+@app.put("/books/update_book")
+async def update_book(updated_book = Body()):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == updated_book.get('title').casefold():
+            BOOKS[i] = updated_book
+
+# Request Body{
+#     "title": "Title Six",
+#     "author": "Author Two",
+#     "category": "History"
+#   }
+# Request URL
+# http://localhost:8000/books
+# Replaces the category of title six
+# check by calling the get api of all books
+            
