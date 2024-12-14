4a2cc8f5468d99d78cd86300f3fcbad3cf8b0e99
preeti-prakash <preetiprakashkandregula@gmail.com>
2024-12-13 23:07:48 -0500

    Mysql setup and api implementations

--- project3/TodoApp/database.py
+++ project3/TodoApp/database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:test1234!@localhost:5432/TodoApplicationDatabase'
+# SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:test1234!@localhost:5432/TodoApplicationDatabase'
+SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:test1234!@127.0.0.1:3306/TodoApplicationDatabase'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
